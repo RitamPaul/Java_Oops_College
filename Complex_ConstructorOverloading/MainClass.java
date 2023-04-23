190d2bcc42729a06df685a008ecd9ec6ed9d2d1b
RitamPaul <ritampaulblogpost@gmail.com>
2023-04-23 15:37:30 +0530

    updated

--- Complex_ConstructorOverloading/MainClass.java
+++ Complex_ConstructorOverloading/MainClass.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 class Complex{
-    private float real, imag;
+    public float real, imag;
     Complex(){
-        real=0, imag=0;
+        real=0;
+        imag=0;
     }
-    Complex(float r, float i){
-        this.real=r;    this.imag=i;
+    Complex(String r, String i){
+        this();
+        if(r!="")   this.real=Float.parseFloat(r);
+        if(i!="")   this.imag=Float.parseFloat(i);
     }
     Complex(Complex obj){
         this.real=obj.real;
@@ -36,13 +40,41 @@ class Complex{
 
 class Main{
     public static void main(String args[]) throws IOException{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        Scanner input = new Scanner(System.in);
         Complex obj1=new Complex(), obj2=new Complex();
 
-        System.out.print("Enter the real part of the 1st complex number = ");
-        String r = input.readLine();
-        System.out.print("Enter the imaginary part of the 1st complex number = ");
-        String i = input.readLine();
-        if(r=="")   obj1=New()
+        System.out.print("\nEnter the real part of 1st complex number = ");
+        String r = input.nextLine();
+        System.out.print("Enter the imaginary part of 1st complex number = ");
+        String i = input.nextLine();
+        obj1 = new Complex(r, i);
+
+        System.out.print("Want to copy previous values & create another complex number ? (1/0) = ");
+        int ch = input.nextInt();
+        input.nextLine();
+        if(ch==1){
+            obj2 = new Complex(obj1);
+        }
+        else{
+            System.out.print("Enter the real part of 2nd complex number = ");
+            r = input.nextLine();
+            System.out.print("Enter the imaginary part of 2nd complex number = ");
+            i = input.nextLine();
+            obj2 = new Complex(r, i);
+        }
+
+        Complex ans = new Complex();
+        System.out.println("\nThe results are :-");
+        ans = ans.add(obj1, obj2);
+        System.out.println("("+obj1.real+" + "+obj1.imag+"i) + ("+obj2.real+" + "+obj2.imag+"i) = ("+ans.real+" + "+ans.imag+"i)");
+
+        ans = ans.mul(obj1, obj2);
+        System.out.println("("+obj1.real+" + "+obj1.imag+"i) * ("+obj2.real+" + "+obj2.imag+"i) = ("+ans.real+" + "+ans.imag+"i)");
+
+        ans = ans.sub(obj1, obj2);
+        System.out.println("("+obj1.real+" + "+obj1.imag+"i) - ("+obj2.real+" + "+obj2.imag+"i) = ("+ans.real+" + "+ans.imag+"i)");
+
+        input.close();
     }
 }
